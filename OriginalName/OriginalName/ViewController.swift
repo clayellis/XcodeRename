d8ca60e362a24a125ad771661ff7a62ec47a6095
Clay Ellis <clayellis@me.com>
2017-07-25 08:33:37 -0600

    Add original project

---
+++ OriginalName/OriginalName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OriginalName
+//
+//  Created by Clay Ellis on 7/25/17.
+//  Copyright © 2017 CUWCD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
